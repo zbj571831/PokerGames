cdc13aaf71d111cc76c8ffdf31fad255c25b5181
abcd1824 <zbj571831@gmail.com>
2026-08-19 23:19:48 +0800

    class03:卡牌顯示器腳本(TMPro輔助)

--- Assets/Scripts/CradDemo.cs
+++ Assets/Scripts/CradDemo.cs
@@ -6,12 +6,15 @@ public class CradDemo : MonoBehaviour
     //宣告類型 名稱 = 新建 實體();
     public PlayingCard testCard = new PlayingCard(Suit.Spades,
         Rank.King);
+    public CardView cardView;
     
 
     void Start()
     {
         //利用除錯訊息視窗印出指定內容
         Debug.Log(testCard.Info());
+        //視覺顯示 綁定(卡牌資料)
+        cardView.Bind(testCard);
     }
 
     
---
+++ Assets/Scripts/View/CardView.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PokerGame.Core
+{
+    public class CardView : MonoBehaviour
+    {
+        #region UI元件
+        [SerializeField] //可在Unity編輯器面板顯示
+        private TMP_Text _suitLabel;
+        [SerializeField]
+        private TMP_Text _rankLabel;
+        #endregion UI元件
+
+        #region 公開方法
+        public void Bind(PlayingCard card)
+        {
+            _suitLabel.text = card.Suit.ToString();  // 顯示花色
+            _rankLabel.text = card.Rank.ToString();  // 顯示點數
+        }
+        #endregion 公開方法
+
+        #region 私有方法
+        #endregion 私有方法
+    }
+
+}
