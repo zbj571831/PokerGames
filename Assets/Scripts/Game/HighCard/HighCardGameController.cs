df7cef9ce4d21e53ec07e39dc72031128c9773ef
abcd1824 <zbj571831@gmail.com>
2026-09-17 22:50:59 +0800

    class05:比大小的MVP

---
+++ Assets/Scripts/Game/HighCard/HighCardGameController.cs
@@ -0,0 +1,33 @@
+﻿using PokerGame.Game;
+using UnityEngine;
+
+public class HighCardGameController : MonoBehaviour
+{
+　　#region 欄位
+    [SerializeField]
+    private Dealer _dealer;
+    [SerializeField]
+    private Transform _playerHand;
+    [SerializeField]
+    private Transform _dealerHand;
+    #endregion 欄位
+    void Start()
+    {
+        PlayRound();
+    }
+
+    /// <summary>
+    ///遊玩回合 
+    /// </summary>
+    #region 公開方法
+    public void PlayRound()
+    {
+        //荷官開局
+        _dealer.BeginRound();
+        //發牌給參與者
+        _dealer.DealTo(_playerHand);
+        _dealer.DealTo(_dealerHand);
+    }
+    #endregion 公開方法
+
+}
